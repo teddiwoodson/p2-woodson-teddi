42729fa8550968765f7ca0517ecf83db180b178b
Teddi Woodson <teddiwood97@ufl.edu>
2019-11-08 08:11:32 -0500

    Add accordion to js

--- main.js
+++ main.js
@@ -6,3 +6,24 @@ function toggleMenu() {
 }
 
 hamburgerButton.addEventListener('click', toggleMenu);
+
+//accordion
+var accordionHeaders = document.querySelectorAll('#accordion .accordion-headers');
+var accordionDescriptions = document.querySelectorAll('#accordion .accordion-description');
+
+//handles accordion dropdown action
+function expandAccordionDescription() {
+
+  //handles toggle indicator
+  for (i = 0; i < accordionHeaders.length; i++) {
+    accordionHeaders[i].childNodes[3].innerHTML = '+';
+    accordionDescriptions[i].classList.remove('accordion-open');
+  }
+
+  this.nextElementSibling.classList.add('accordion-open');
+  this.childNodes[3].innerHTML = '-';
+}
+//add event listener for accordion
+for (i = 0; i < accordionHeaders.length; i++) {
+  accordionHeaders[i].addEventListener('click', expandAccordionDescription);
+}
