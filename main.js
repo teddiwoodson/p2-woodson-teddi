5861991c841ad3ad8ff92f85e640ad1c2cf04383
Teddi Woodson <teddiwood97@ufl.edu>
2019-11-08 09:07:51 -0500

    Add httpRequest code to main.js

--- main.js
+++ main.js
@@ -27,3 +27,16 @@ function expandAccordionDescription() {
 for (i = 0; i < accordionHeaders.length; i++) {
   accordionHeaders[i].addEventListener('click', expandAccordionDescription);
 }
+
+//API Code
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var apiResult = JSON.parse(this.responseText);
+
+        //Code Dealing With The API Data Goes Here
+
+    }
+};
+xmlhttp.open('GET', 'API URL Goes Here', true);
+xmlhttp.send();
